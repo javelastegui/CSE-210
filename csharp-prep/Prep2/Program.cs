a17e932262fef23fe2f4f164f7090b44c361160f
javelastegui <javelastegui@outlook.com>
2025-01-13 23:53:13 -0700

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string user_grade = Console.ReadLine();
+        int grade = int.Parse(user_grade);
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+                if (grade >= 70)
+        {
+            Console.WriteLine("You approved");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass");
+        }    
     }
 }
