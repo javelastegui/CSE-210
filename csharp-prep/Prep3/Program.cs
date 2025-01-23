c203f6d24f4da413f0ab25d9d79d319d21610cdb
javelastegui <javelastegui@outlook.com>
2025-01-22 22:03:48 -0700

    Abstraction Learning Activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*Console.WriteLine("What is the magic number? ");
+        int response = int.Parse(Console.ReadLine());*/
+
+        Random randomGenerator = new Random();
+        int response = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != response)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > response)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < response)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Job
+{
+  public string _company;
+  public string _jobtitle;
+  public int _startYear;
+  public int _endYear;
+
+  public void Display()
+  {
+    Console.WriteLine($"{_jobtitle} {_company} {_startYear}-{_endYear}");
+  }
+}
